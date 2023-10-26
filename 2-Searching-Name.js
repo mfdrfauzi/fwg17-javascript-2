070df97d697f57be2fba669f2724132449fc8cef
Moch. Fauzi <109473978+mfdrfauzi@users.noreply.github.com>
2023-10-27 03:30:52 +0700

    Revisi

--- 2-Searching-Name.js
+++ 2-Searching-Name.js
@@ -1,17 +1,86 @@
+const toLowerCase = (str) => {
+    let result = '';
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        let charIndex = -1;
+        for (let j = 0; j < uppercaseChars.length; j++) {
+            if (char === uppercaseChars[j]) {
+                charIndex = j;
+                break;
+            }
+        }
+        if (charIndex !== -1) {
+            char = lowercaseChars[charIndex];
+        }
+        result += char;
+    }
+    return result;
+}
+
+const manualIncludes = (text, search) => {
+    for (let i = 0; i <= text.length - search.length; i++) {
+        let found = true;
+        for (let j = 0; j < search.length; j++) {
+            if (text[i + j] !== search[j]) {
+                found = false;
+                break;
+            }
+        }
+        if (found) {
+            return true;
+        }
+    }
+    return false;
+}
+
 const name = [
     'Abigail', 'Alexandra', 'Alison',
     'Amanda', 'Angela', 'Bella',
     'Carol', 'Caroline', 'Carolyn',
     'Deirdre', 'Diana', 'Elizabeth',
-    'Ella', 'Faith', 'Olivia', 'Penelope'];
+    'Ella', 'Faith', 'Olivia', 'Penelope'
+];
 
-const searchName = (search, limit, cb) => {
-    let hasil = name.filter(name => name.toLowerCase().includes(search.toLowerCase())).slice(0,limit)
-    cb(hasil)
-}    
-
-const hasilSearch = (hasil) => {
-    console.log(hasil)  
+const filterNamesBySearch = (search, limit) => {
+    const hasil = [];
+    for (let i = 0; i < name.length; i++) {
+        let nameLower = toLowerCase(name[i]);
+        let searchLower = toLowerCase(search);
+        if (manualIncludes(nameLower, searchLower)) {
+            hasil[hasil.length] = name[i];
+            if (hasil.length >= limit) {
+                break;
+            }
+        }
+    }
+    return hasil;
 }
 
-searchName("an",3,hasilSearch)
+const searchNames = (search, limit) => {
+    const hasil = filterNamesBySearch(search, limit);
+    return console.log(hasil)
+}
+
+searchNames("an", 3);
+
+
+// dengan built-in Function
+// const name = [
+//     'Abigail', 'Alexandra', 'Alison',
+//     'Amanda', 'Angela', 'Bella',
+//     'Carol', 'Caroline', 'Carolyn',
+//     'Deirdre', 'Diana', 'Elizabeth',
+//     'Ella', 'Faith', 'Olivia', 'Penelope'];
+
+// const searchName = (search, limit, cb) => {
+//     let hasil = name.filter(name => name.toLowerCase().includes(search.toLowerCase())).slice(0,limit)
+//     cb(hasil)
+// }    
+
+// const hasilSearch = (quote) => {
+//     console.log(quote)  
+// }
+
+// searchName("an",3,hasilSearch)
--- 3-Seleksi-Nilai.js
+++ 3-Seleksi-Nilai.js
@@ -1,22 +1,58 @@
-const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) =>{
-    if (nilaiAwal > nilaiAkhir){
-        return console.log("Nilai akhir harus lebih besar dari nilai awal!")
+const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
+    if (nilaiAwal > nilaiAkhir) {
+        return console.log("Nilai akhir harus lebih besar dari nilai awal!");
     }
-
-    if (dataArray.length <= 5){
-        return console.log("Jumlah angka dalam dataArray harus lebih dari 5")
+    if (dataArray.length <= 5) {
+        return console.log("Jumlah angka dalam dataArray harus lebih dari 5");
     }
-
-    const hasilSeleksi = dataArray.filter((nilai) => nilai >= nilaiAwal && nilai <= nilaiAkhir)
-
-    if (hasilSeleksi.length === 0){
-        return console.log("Nilai tidak ditemukan")
+    const hasilSeleksi = [];
+    for (let i = 0; i < dataArray.length; i++) {
+        const nilai = dataArray[i];
+        if (nilai >= nilaiAwal && nilai <= nilaiAkhir) {
+            hasilSeleksi[hasilSeleksi.length] = nilai;
+        }
     }
-
-    return console.log(hasilSeleksi.sort())
+    if (hasilSeleksi.length === 0) {
+        return console.log("Nilai tidak ditemukan");
+    }
+    for (let i = 0; i < hasilSeleksi.length - 1; i++) {
+        for (let j = i + 1; j < hasilSeleksi.length; j++) {
+            if (hasilSeleksi[i] > hasilSeleksi[j]) {
+                const temp = hasilSeleksi[i];
+                hasilSeleksi[i] = hasilSeleksi[j];
+                hasilSeleksi[j] = temp;
+            }
+        }
+    }
+    return console.log(hasilSeleksi);
 }
 
-seleksiNilai(5, 20 , [2, 25, 4, 14, 17, 30, 8])
-seleksiNilai(15, 3 , [2, 25, 4, 14, 17, 30, 8])
-seleksiNilai(4, 17 , [2, 25, 4])
-seleksiNilai(5, 17 , [2, 25, 4, 1, 30, 18])
+seleksiNilai(5, 20 , [2, 25, 4, 14, 17, 30, 8]);
+seleksiNilai(15, 3 , [2, 25, 4, 14, 17, 30, 8]);
+seleksiNilai(4, 17 , [2, 25, 4]);
+seleksiNilai(5, 17 , [2, 25, 4, 1, 30, 18]);
+
+
+// dengan Built-in Function
+// const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) =>{
+//     if (nilaiAwal > nilaiAkhir){
+//         return console.log("Nilai akhir harus lebih besar dari nilai awal!")
+//     }
+
+//     if (dataArray.length <= 5){
+//         return console.log("Jumlah angka dalam dataArray harus lebih dari 5")
+//     }
+
+//     const hasilSeleksi = dataArray.filter((nilai) => nilai >= nilaiAwal && nilai <= nilaiAkhir)
+
+//     if (hasilSeleksi.length === 0){
+//         return console.log("Nilai tidak ditemukan")
+//     }
+
+//     return console.log(hasilSeleksi.sort())
+// }
+
+// seleksiNilai(5, 20 , [2, 25, 4, 14, 17, 30, 8])
+// seleksiNilai(15, 3 , [2, 25, 4, 14, 17, 30, 8])
+// seleksiNilai(4, 17 , [2, 25, 4])
+// seleksiNilai(5, 17 , [2, 25, 4, 1, 30, 18])
